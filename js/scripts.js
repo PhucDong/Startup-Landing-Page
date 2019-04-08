5da9bd841efb62da4ff59e59741e9dcd0ca74b19
Dong Vu Minh Phuc <dvmp1014@gmail.com>
2019-04-08 14:54:54 +0700

    Scrolling effect for header

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
-let menuIcon = document.getElementById("menu-icon");
-
 const secondMenu = () => {
-    let secondNavList = document.getElementById("second-nav-list");
+    const secondNavList = document.getElementById("second-nav-list");
+
     if (secondNavList.style.display === "none") {
         secondNavList.style.display = "block";
     }
@@ -9,4 +8,28 @@ const secondMenu = () => {
         secondNavList.style.display = "none";
     }
 }
+
+const menuIcon = document.getElementById("menu-icon");
 menuIcon.addEventListener("click", secondMenu);
+
+
+window.addEventListener("scroll", () => {
+    const header = document.querySelector("header");
+    const brandHeading = document.getElementById("site-brand__heading");
+    const navLinks = document.querySelectorAll(".nav-link");
+
+    if (document.documentElement.scrollTop > 100 || document.body.scrollTop > 100) {
+        header.style.backgroundColor = "#fff";
+        brandHeading.style.color = "#000";
+        navLinks.forEach(item => {
+            item.style.color = "#000";
+        })
+    }
+    else {
+        header.style.backgroundColor = "transparent";
+        brandHeading.style.color = "#fff";
+        navLinks.forEach(item => {
+            item.style.color = "#fff";
+        })
+    }
+})
